38a8cebbcf56a1c08df687e63309dc750201dbf9
jell <euler31415@icloud.com>
2021-03-28 00:21:46 +0900

    log

--- bundle.py
+++ bundle.py
@@ -1,16 +1,16 @@
-import onlinejudge_bundle
 import sys
 import os
 import subprocess
-import glob
 import pathlib
-from logging import getLogger, INFO, basicConfig
+from logging import getLogger
 logger = getLogger(__name__)
+
+logf = '.bundlelog'
 
 
 def main(args):
     if not len(args):
-        logger.error('\033[31m' + " no source file specified.")
+        logger.error('\033[31m' + " No source file specified.")
         sys.exit(1)
 
     workpath = os.path.expanduser('~')
@@ -24,10 +24,23 @@ def main(args):
 
     if len(args) > 1:
         stdout = args[1]
-        with open(args[1], "w") as out:
-            exit_stat = subprocess.call(cmd, cwd=workpath, stdout=out, stderr=subprocess.DEVNULL)
-        logger.info(" {} -> {}".format(args[0], stdout))
+        with open(args[1], 'w') as out, open(logf, 'w') as err:
+            exit_stat = subprocess.call(cmd, cwd=workpath, stdout=out, stderr=err)
+        if exit_stat:
+            with open(logf, 'r') as err:
+                for msg in err.readlines():
+                    logger.info(msg)
+        else:
+            logger.info(" {} -> {}".format(args[0], stdout))
+
     else:
-        exit_stat = subprocess.call(cmd, cwd=workpath, stdout=None, stderr=subprocess.DEVNULL)
+        with open(logf, 'w') as err:
+            exit_stat = subprocess.call(cmd, cwd=workpath, stdout=None, stderr=err)
+        if exit_stat:
+            with open(logf, 'r') as err:
+                for msg in err.readlines():
+                    logger.info(msg)
+
+    os.remove(logf)
 
     return exit_stat
